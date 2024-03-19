9a4888333d14f064dc252c71b4ece053466af6ac
Carlos Román Alcaide <cromanalcaidev@gmail.com>
2024-03-19 17:22:27 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Carlos Román Alcaide on 19/3/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
